e6b65e3814d3f1095b26b0fb8157d7339984ce8d
孔林 <konglin@aodiansoft.com>
2017-01-06 19:31:15 +0800

    增加 C# SDK

--- sdk/php/test.php
+++ sdk/php/test.php
@@ -8,7 +8,7 @@ $test_1 = HttpApi::post('RoomMgr', 'getAuthToken', array(
     'nick' => 'ttt',
     'ava' => ''
 ));
-echo 'teste1 ', json_encode($test_1), "\n";
+echo 'test_1 ', json_encode($test_1), "\n";
 
 
 $test_2 = HttpApi::post('JiaoYuUserInfo', 'listWisDoc', array(
